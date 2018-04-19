69788be73739f9ce925d7eaebd69444f7abfd72c
Matej Mitas <matej.mitas@gmail.com>
2018-04-19 09:16:07 +0200

    Added new module for handling JSON files

---
+++ filemanager.js
@@ -0,0 +1,18 @@
+/*
+Handling JSON input files
+@project: SQL Generator
+@brief: Handling JSON input files
+@author: Matej Mitas
+@file: file.js
+*/
+
+const fs = require('fs');
+const File = require('./file.js');
+
+module.exports = class FileManager {
+	// we load a JSON file, an directly parse
+	// it into JS string
+	constructor(path) {
+		
+	}
+}
